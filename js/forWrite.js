39fa57944bba482f67956865bd45cb181e8e8a4c
Igor <igorvinson@gmail.com>
2022-05-16 15:39:35 -0600

    second practice

---
+++ js/forWrite.js
@@ -0,0 +1,22 @@
+let result = '';
+let lines = 5;
+for (let i = 0; i <= lines; i++) {
+    for (let j = 0; j <lines - i; j++) {
+        result +=' '; }
+    for (let j = 0; j < (2*i + 1); j++) {
+        result += '*'
+    }
+    result += '\n';
+}
+console.log(result);
+
+
+
+
+
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+//***********
